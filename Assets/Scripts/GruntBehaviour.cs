6a56614162b7f64f40a6c2725ced082780956d79
Angus <1034241@student.sae.edu.au>
2025-07-05 00:52:26 +1000

    changes to enemy movement

--- Assets/Scripts/GruntBehaviour.cs
+++ Assets/Scripts/GruntBehaviour.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class GruntBehaviour : MonoBehaviour
 {
+    // target container for player transform, set in inspector
     public Transform target;
-    
+
+    //getting rigid body in case it gets used
+    Rigidbody rb;
+
+    // serialized speed for debug
+    [SerializeField]
+    float speed = 1.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,9 +28,23 @@ public class GruntBehaviour : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        // Null reference check for player position target
         if (target != null)
         {
             transform.LookAt(target.position);
+            GruntMovement();
+        }
+    }
+
+    void GruntMovement()
+    {
+        // Null reference check
+        if (target != null)
+        {
+            // new temp variable for using in the vector3 operation
+            float moveSpeed = speed * Time.deltaTime;
+            // changes the position to move toward the player at Movespeed
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed);
         }
     }
 }
