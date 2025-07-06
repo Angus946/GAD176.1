52d939d61888bd36487b2aaed6a824883f08c5fe
Angus <1034241@student.sae.edu.au>
2025-07-06 14:19:03 +1000

    playermovement from unity

---
+++ Assets/Scripts/HitScan.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScan : MonoBehaviour
+{
+    float rangeHS;
+    float damageHS;
+    public Transform transformHS;
+    public Camera cam;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transformHS != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }
+    }
+
+    public void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transformHS.position, transformHS.forward, out hit, rangeHS) )
+        {
+            Debug.Log(hit.transform.name);
+
+        }
+       
+    }
+}
